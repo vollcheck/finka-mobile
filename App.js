e88929001f79221bf097993f531160d5fdeb48bb
User <jaw@juxt.pro>
2023-02-25 15:01:21 +0100

    deletion works, editing requires more commitment

--- App.js
+++ App.js
@@ -12,22 +12,41 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   const [expenses, setExpenses] = useState(["food", "clothes"]);
   const [inputValue, setInputValue] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
 
   const handleAddExpense = () => {
     if (inputValue.trim() !== "") {
-      setExpenses([...expenses, inputValue.trim()]);
-      setInputValue("");
+      if (editIndex !== null) {
+        expenses[editIndex] = inputValue.trim();
+        setExpenses([...expenses]);
+        setInputValue("");
+        setEditIndex();
+      } else {
+        setExpenses([...expenses, inputValue.trim()]);
+        setInputValue("");
+      }
     }
   };
 
+  const handleEditExpense = (index) => {
+    setInputValue(expenses[index]);
+    setEditIndex(index);
+  }
+
+  const handleDeleteExpense = (index) => {
+    expenses.splice(index, 1);
+    setExpenses([...expenses]);
+  }
+
   return (
     <View style={styles.container}>
-
-      <View>
-        {expenses.map((a, index) => (
-          <ListItem title={a} key={index} />
-        ))}
-      </View>
+      {expenses.map((expense, index) => (
+        <View key={index}>
+          <ListItem title={expense} />
+          <Button title="Edit" onPress={() => handleEditExpense(index)} />
+          <Button title="Delete" onPress={() => handleDeleteExpense(index)} />
+        </View>
+      ))}
 
       <TextInput
         label="Enter expense name"
@@ -35,8 +54,10 @@ export default function App() {
         value={inputValue}
         onChangeText={setInputValue}
       />
-      <Button title="Add expense" onPress={handleAddExpense} />
-
+      <Button
+        title={editIndex !== null ? "Edit expense" : "Add expense" }
+        onPress={handleAddExpense}
+      />
     </View>
   );
 }
