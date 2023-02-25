4c79416715df3d631496566c0b1e1674c905cb8f
User <jaw@juxt.pro>
2023-02-25 14:47:12 +0100

    init - expense creation is possible

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Badge,
+  Button,
+  Chip,
+  ListItem,
+  Stack,
+  TextInput,
+} from "@react-native-material/core";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [expenses, setExpenses] = useState(["food", "clothes"]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddExpense = () => {
+    if (inputValue.trim() !== "") {
+      setExpenses([...expenses, inputValue.trim()]);
+      setInputValue("");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View>
+        {expenses.map((a, index) => (
+          <ListItem title={a} key={index} />
+        ))}
+      </View>
+
+      <TextInput
+        label="Enter expense name"
+        variant="standard"
+        value={inputValue}
+        onChangeText={setInputValue}
+      />
+      <Button title="Add expense" onPress={handleAddExpense} />
+
     </View>
   );
 }
@@ -13,8 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
